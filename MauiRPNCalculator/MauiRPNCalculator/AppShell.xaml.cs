0c25428b608ac2a92dc0306ebb8953460f03a044
Bruno Kuhn <brunofernandes20003@gmail.com>
2024-10-20 10:22:54 -0300

    Cria o projeto Maui

---
+++ MauiRPNCalculator/MauiRPNCalculator/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiRPNCalculator
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ MauiRPNCalculator/MauiRPNCalculator/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiRPNCalculator
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
